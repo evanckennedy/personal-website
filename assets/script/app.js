74a85ac99df615170e3a35f597382bf047f6b90c
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-08 09:53:45 -0600

    initial commit

---
+++ assets/script/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// This app requires a server to handle import statements
+// and CORS issues
+import * as utils from './utils.js';
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Organizer                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
