7e48ec216ae09e169ceed742471b91084fc2bb6c
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-11 09:51:24 -0500

    make contact cards clickable

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,6 +6,34 @@ import * as utils from './utils.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
+/* Element selection                                     */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const linkedinCard = document.querySelector('.linkedin-card');
+const gitHubCard = document.querySelector('.github-card');
+const whatsAppCard = document.querySelector('.whatsapp-card');
+const emailCard = document.querySelector('.email-card');
+
+const linkedinLink = "https://www.linkedin.com/in/evanckennedy";
+const gitHubLink = "https://github.com/evanckennedy";
+const whatsAppLink = "https://wa.me/12044302548";
+const emailLink = "mailto:evanckennedy@protonmail.com";
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/* Make cards clickable                                  */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+linkedinCard.addEventListener('click', () => {
+  window.location.href = linkedinLink;
+});
+
+gitHubCard.addEventListener('click', () => {
+  window.location.href = gitHubLink;
+});
+
+whatsAppCard.addEventListener('click', () => {
+  window.location.href = whatsAppLink;
+});
+
+emailCard.addEventListener('click', () => {
+  window.location.href = emailLink;
+});
